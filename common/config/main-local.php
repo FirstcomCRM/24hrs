cd8d8c9f6b3cf178266f89e856d1b677d67a9a4b
Eumaser <eumerjoseph.ramos@yahoo.com>
2017-11-16 17:07:31 +0800

    added swiftmailer

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -19,7 +19,19 @@ return [
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
-            'useFileTransport' => true,
+            'useFileTransport' => false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+              
+                'port' => '465',
+                'encryption' => 'ssl',
+                'streamOptions'=> [ 'ssl' =>
+                    [ 'allow_self_signed' => true,
+                        'verify_peer' => false,
+                        'verify_peer_name' => false,
+                    ],
+                ] //here
+            ],
         ],
     ],
 ];
