15a419fda95fa8e18539b2fb3ba81ba75ffd4a9f
Eumaser <eumerjoseph.ramos@yahoo.com>
2017-11-17 18:03:57 +0800

    added JoinWith and filters

--- common/models/OrderSearch.php
+++ common/models/OrderSearch.php
@@ -16,12 +16,15 @@ class OrderSearch extends Order
      * @inheritdoc
      */
     public $delivery_date;
+    public $start;
+    public $end;
+    public $product_code;
 
     public function rules()
     {
         return [
             [['order_id', 'invoice_no', 'store_id', 'customer_id', 'customer_group_id', 'payment_country_id', 'payment_zone_id', 'shipping_country_id', 'shipping_zone_id', 'order_status_id', 'affiliate_id', 'marketing_id', 'language_id', 'currency_id'], 'integer'],
-            [['order_source', 'invoice_prefix', 'store_name', 'store_url', 'firstname', 'lastname', 'email', 'telephone', 'shipping_telephone', 'fax', 'custom_field', 'payment_firstname', 'payment_lastname', 'payment_company', 'payment_address_1', 'payment_address_2', 'payment_city', 'payment_postcode', 'payment_country', 'payment_zone', 'payment_address_format', 'payment_custom_field', 'payment_method', 'payment_code', 'shipping_firstname', 'shipping_lastname', 'shipping_company', 'shipping_address_1', 'shipping_address_2', 'shipping_city', 'shipping_postcode', 'shipping_country', 'shipping_zone', 'shipping_address_format', 'shipping_custom_field', 'shipping_method', 'shipping_code', 'comment', 'tracking', 'currency_code', 'ip', 'forwarded_ip', 'user_agent', 'accept_language', 'date_added', 'date_modified', 'date_invoice','delivery_date'], 'safe'],
+            [['order_source', 'invoice_prefix', 'store_name', 'store_url', 'firstname', 'lastname', 'email', 'telephone', 'shipping_telephone', 'fax', 'custom_field', 'payment_firstname', 'payment_lastname', 'payment_company', 'payment_address_1', 'payment_address_2', 'payment_city', 'payment_postcode', 'payment_country', 'payment_zone', 'payment_address_format', 'payment_custom_field', 'payment_method', 'payment_code', 'shipping_firstname', 'shipping_lastname', 'shipping_company', 'shipping_address_1', 'shipping_address_2', 'shipping_city', 'shipping_postcode', 'shipping_country', 'shipping_zone', 'shipping_address_format', 'shipping_custom_field', 'shipping_method', 'shipping_code', 'comment', 'tracking', 'currency_code', 'ip', 'forwarded_ip', 'user_agent', 'accept_language', 'date_added', 'date_modified', 'date_invoice','delivery_date','start','end','product_code'], 'safe'],
             [['total', 'commission', 'currency_value'], 'number'],
         ];
     }
@@ -50,20 +53,22 @@ class OrderSearch extends Order
       //  $query->joinWith(['orderProduct'])->select(['order.order_id,order.invoice_no,order.invoice_prefix.order.order_status_id']);
 
       //this one to use
-    /* $query->joinWith(['orderProduct'])->select('order.order_id,order.invoice_no,order.invoice_prefix,order.order_status_id,order_product.delivery_date,
-      order_product.delivery_text_time,order_product.product_id'); */
-
-    //  $query->joinWith(['orderProduct'])->select('order.order_id,order.invoice_no,order.invoice_prefix,order.order_status_id,order_product.delivery_date');
-      //samp;e
-      //$query = Item::find()->joinWith(['subcategory'])->select(['item.*', 'sub_category.name'])->where(['item.active' => 1])->addOrderBy(['item.id' => SORT_DESC]);
-        // add conditions that should always apply here
+      $query->joinWith(['orderProduct'])->select('order.order_id,order.invoice_no,order.invoice_prefix,order.order_status_id,order_product.delivery_date,
+      order_product.product_id');
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
+            'pagination'=>[
+              'pageSize'=>10,
+            ],
         ]);
 
         $query->orderBy(['order_id'=>SORT_DESC]);
         $this->load($params);
+
+        if (!empty($this->delivery_date)) {
+          list($this->start,$this->end)= explode(' - ',$this->delivery_date);
+        }
 
         if (!$this->validate()) {
             // uncomment the following line if you do not want to return any records when validation fails
@@ -76,24 +81,8 @@ class OrderSearch extends Order
         //    'Order.order_id' => $this->order_id,
             'order_id' => $this->order_id,
             'invoice_no' => $this->invoice_no,
-            'store_id' => $this->store_id,
-            'customer_id' => $this->customer_id,
-            'customer_group_id' => $this->customer_group_id,
-            'payment_country_id' => $this->payment_country_id,
-            'payment_zone_id' => $this->payment_zone_id,
-            'shipping_country_id' => $this->shipping_country_id,
-            'shipping_zone_id' => $this->shipping_zone_id,
-            'total' => $this->total,
-            'order_status_id' => $this->order_status_id,
-            'affiliate_id' => $this->affiliate_id,
-            'commission' => $this->commission,
-            'marketing_id' => $this->marketing_id,
-            'language_id' => $this->language_id,
-            'currency_id' => $this->currency_id,
-            'currency_value' => $this->currency_value,
-            'date_added' => $this->date_added,
-            'date_modified' => $this->date_modified,
-            'date_invoice' => $this->date_invoice,
+            'order_status_id'=>$this->order_status_id,
+
         ]);
 
         $query->andFilterWhere(['like', 'order_source', $this->order_source])
@@ -103,11 +92,9 @@ class OrderSearch extends Order
             ->andFilterWhere(['like', 'firstname', $this->firstname])
             ->andFilterWhere(['like', 'lastname', $this->lastname])
             ->andFilterWhere(['like', 'email', $this->email])
-            ->andFilterWhere(['like', 'telephone', $this->telephone])
-            ->andFilterWhere(['like', 'shipping_telephone', $this->shipping_telephone])
-            ->andFilterWhere(['like', 'fax', $this->fax]);
-        //   ->andFilterWhere(['like', 'order_product.delivery_date', $this->delivery_date]);
-
+        //    ->andFilterWhere(['like', 'order_product.delivery_date', $this->delivery_date])
+          ->andFilterWhere(['between', 'order_product.delivery_date', $this->start, $this->end])
+            ->andFilterWhere(['like', 'order_product.product_id', $this->product_code]);
         return $dataProvider;
     }
 
