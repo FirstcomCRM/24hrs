bc318de7473d18a2fbfaf46da5db79422c061377
Eumaser <eumerjoseph.ramos@yahoo.com>
2017-11-20 14:40:41 +0800

    changes to the login page

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -11,14 +11,19 @@ $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>Please fill out the following fields to login:</p>
 
     <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+        <div class="col-md-3">
 
+        </div>
+
+        <div class="col-md-6">
+          <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                <h3 class="panel-title">24 Hrs Order System</h3>
+              </div>
+              <div class="panel-body">
                 <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
 
                 <?= $form->field($model, 'password')->passwordInput() ?>
@@ -26,10 +31,19 @@ $this->params['breadcrumbs'][] = $this->title;
                 <?= $form->field($model, 'rememberMe')->checkbox() ?>
 
                 <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                    <?= Html::submitButton('<i class="fa fa-sign-in" aria-hidden="true"></i> Login', ['class' => 'btn btn-default', 'name' => 'login-button']) ?>
                 </div>
+              </div>
+              <div class="panel-footer text-center">
+                @<?php echo date('Y') ?> Powered by <?php echo Html::a('Firstcom Solutions','https://www.firstcom.com.sg/'); ?>
+              </div>
+            </div>
 
-            <?php ActiveForm::end(); ?>
+
+          <?php ActiveForm::end(); ?>
+        </div>
+        <div class="col-md-3">
+
         </div>
     </div>
 </div>
