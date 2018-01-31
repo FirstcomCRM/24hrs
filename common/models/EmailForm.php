e00e190d4ce477dbd5cccc75c97ea8994ca8eaf5
Eumaser <eumerjoseph.ramos@yahoo.com>
2018-01-31 10:47:02 +0800

    email form model

---
+++ common/models/EmailForm.php
@@ -0,0 +1,32 @@
+<?php
+namespace common\models;
+
+use Yii;
+use yii\base\Model;
+
+/**
+ * Login form
+ */
+class EmailForm extends Model
+{
+    public $email;
+    public $title;
+    public $cc;
+    public $message;
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+
+            [['email', 'title','message'], 'required'],
+            [['cc'],'string'],
+            [['email'],'email'],
+
+        ];
+    }
+
+
+}
