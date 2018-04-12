6cb48acec0fa51fd21caaf3cfe9b2ac6fc248d16
Eumaser <eumerjoseph.ramos@yahoo.com>
2018-04-12 15:07:37 +0800

    added new function for canceled orders

--- common/models/OrderSearch.php
+++ common/models/OrderSearch.php
@@ -288,7 +288,7 @@ class OrderSearch extends Order
           ->leftJoin('offline_order_product b','b.off_order_id=a.id')
           ->leftJoin('delivery_time fu','fu.id=a.delivery_time')
           ->groupBy(['a.id'])
-          ->where(['a.status'=>1]);
+          ->where(['a.status'=>5]);
 
       $query_on = (new \yii\db\Query())
           ->select('c.order_id,c.invoice_no,c.order_status_id,c.date_invoice,d.delivery_date,d.delivery_text_time,d.product_id')
@@ -297,8 +297,91 @@ class OrderSearch extends Order
           ->groupBy(['c.order_id'])
           ->limit(500)
           ->orderBy(['d.delivery_date'=>SORT_DESC])
-          ->where(['c.order_status_id'=>5])
-          ->orWhere(['c.order_status_id'=>7]);
+          ->where(['c.order_status_id'=>5]);
+          //->orWhere(['c.order_status_id'=>7]);
+
+    //      ->andWhere(['!=','d.delivery_date','0000-00-00']);
+
+      $query = (new \yii\db\Query())
+          ->from(['dummy_name' => $query_off->union($query_on)]);
+          //  ->orderBy(['del_date' => SORT_DESC]);
+
+        //  echo '<pre>';
+        //  print_r($query);
+
+
+        $dataProvider = new ActiveDataProvider([
+              'query' => $query,
+          //    'sort'=> ['defaultOrder' => ['dadst'=>SORT_DESC]]
+
+        ]);
+
+        $dataProvider->setSort([
+          'defaultOrder' => ['del_date'=>SORT_DESC],
+          'attributes'=>[
+            'id',
+            'invoice_no',
+            'invoice_date',
+            'del_date',
+          ],
+        ]);
+
+        $this->load($params);
+
+
+         //print_r($this->start);die();
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->order_id,
+            'invoice_date' => $this->invoice_date,
+        //    'del_date' => $this->delivery_date,
+        ]);
+
+        if (!empty($this->delivery_date)) {
+          list($this->start,$this->end)= explode(' - ',$this->delivery_date);
+          $this->start =  date('Y-m-d', strtotime($this->start) );
+          $this->end =  date('Y-m-d', strtotime($this->end ) );
+          $query->andFilterWhere(['between', 'del_date', $this->start, $this->end]);
+        //  die($this->end);
+        }else{
+          /*$this->start = date('Y-m-d');
+          $date = new \DateTime($this->start);
+          $this->start = $date->modify('+2 day')->format('Y-m-d');
+          $this->end = $date->modify('+2 years')->format('Y-m-d');
+          $query->andFilterWhere(['between', 'del_date', $this->start, $this->end]);*/
+        }
+
+        $query->andFilterWhere(['like', 'invoice_no', $this->invoice_no]);
+        //      ->andFilterWhere(['between', 'del_date', $this->start, $this->end]);
+
+        return $dataProvider;
+    }
+
+    public function canceled_search($params){
+    /*  $query_off = (new \yii\db\Query())
+          ->select('a.id,a.invoice_no,a.status,a.invoice_date,b.del_date,b.del_time,b.item_code')
+          ->from('offline_order a')
+          ->leftJoin('offline_order_product b','b.off_order_id=a.id')
+          ->groupBy(['a.id'])
+          ->where(['a.status'=>5])
+          ->orWhere(['a.status'=>7]);*/
+      $query_off = (new \yii\db\Query())
+          ->select('a.id,a.invoice_no,a.status,a.invoice_date,b.del_date,fu.delivery_time as del_time,b.item_code')
+          ->from('offline_order a')
+          ->leftJoin('offline_order_product b','b.off_order_id=a.id')
+          ->leftJoin('delivery_time fu','fu.id=a.delivery_time')
+          ->groupBy(['a.id'])
+          ->where(['a.status'=>7]);
+
+      $query_on = (new \yii\db\Query())
+          ->select('c.order_id,c.invoice_no,c.order_status_id,c.date_invoice,d.delivery_date,d.delivery_text_time,d.product_id')
+          ->from('order c')
+          ->leftJoin('order_product d','d.order_id=c.order_id')
+          ->groupBy(['c.order_id'])
+          ->limit(500)
+          ->orderBy(['d.delivery_date'=>SORT_DESC])
+          ->where(['c.order_status_id'=>7]);
+          //->orWhere(['c.order_status_id'=>7]);
 
     //      ->andWhere(['!=','d.delivery_date','0000-00-00']);
 
