2d3c826fe70bc5c0bb79161e833511aba9cc371e
Eumaser <eumerjoseph.ramos@yahoo.com>
2018-04-16 17:33:46 +0800

    added root alias

--- common/config/params-local.php
+++ common/config/params-local.php
@@ -1,5 +1,6 @@
 <?php
 yii::setAlias('@image', '/image');
-
+yii::setAlias('@cimages', dirname(dirname(__DIR__)) . '/image');
+Yii::setAlias('@roots', realpath(dirname(__FILE__).'/../../../'));
 return [
 ];
