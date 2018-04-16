9da6f1817f784ce154ca05b727a3b1bcd1a5e337
Eumaser <eumerjoseph.ramos@yahoo.com>
2018-04-16 17:30:23 +0800

    added remarks form

---
+++ backend/views/order/_remarks.php
@@ -0,0 +1,19 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use yii\helpers\Url;
+use yii\bootstrap\Modal;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\OfflineOrder */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+
+<?php $form = ActiveForm::begin(['id' => 'dynamic-form']); ?>
+    <?= $form->field($model, 'remarks')->textarea(['rows' => 4]) ?>
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-primary']) ?>
+    </div>
+<?php ActiveForm::end(); ?>
