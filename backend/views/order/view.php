25d2952b34c96914123222e74a0a412958cabac9
Eumaser <eumerjoseph.ramos@yahoo.com>
2018-04-19 10:06:43 +0800

    overhauled order view. Set up like email format

--- backend/views/order/view.php
+++ backend/views/order/view.php
@@ -2,97 +2,186 @@
 
 use yii\helpers\Html;
 use yii\widgets\DetailView;
+use common\models\Order;
+use common\models\OrderProduct;
+use common\models\Product;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Order */
 
-$this->title = $model->order_id;
-$this->params['breadcrumbs'][] = ['label' => 'Orders', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
+//$this->title = $model->order_id;
+//$this->params['breadcrumbs'][] = ['label' => 'Orders', 'url' => ['index']];
+//$this->params['breadcrumbs'][] = $this->title;
+
+$path = '../web/logo/banner-en.jpg';
+$defaults =  '../web/logo/defaults.jpg';
+$npath = '';
+$sums = [];
+
+$parrs = OrderProduct::find()->where(['order_id'=>$model->order_id])->asArray()->all();
+$pone = OrderProduct::find()->where(['order_id'=>$model->order_id])->one();
 ?>
+
+<style>
+  .panel-red{
+    background-color: #A78D8C;
+    color:white;
+    font-weight: bold;
+  }
+  .pheader{
+    color: #40090C;
+    font-weight: bold;
+  }
+  .products{
+    color: #FF0000;
+    font-weight: bold;
+  }
+  .rborder{
+    border-top:0px;
+  }
+
+  table,
+  .order-view{
+    font-family: Arial;
+    font-size: 16px;
+  }
+  hr{
+    border: 1px dotted black;
+    border-style: none none dotted;
+    color: #fff;
+    background-color: #fff;
+  }
+
+
+</style>
+
 <div class="order-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+  <div class="img-responsive">
+    <img src="<?php echo $path ?>" alt="">
+  </div>
 
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->order_id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->order_id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
+  <br>
+  <div class="panel panel-default">
+    <div class="panel-body panel-red">
+      <div class="row">
+        <div class="col-md-6">
+          Thank you <?php echo $model->firstname ?> for your order.
+        </div>
+        <div class="col-md-6 text-right">
+          Order No: #<?php echo $model->order_id ?>
+        </div>
+      </div>
+    </div>
+  </div>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'order_id',
-            'order_source',
-            'invoice_no',
-            'invoice_prefix',
-            'store_id',
-            'store_name',
-            'store_url:url',
-            'customer_id',
-            'customer_group_id',
-            'firstname',
-            'lastname',
-            'email:email',
-            'telephone',
-            'shipping_telephone',
-            'fax',
-            'custom_field:ntext',
-            'payment_firstname',
-            'payment_lastname',
-            'payment_company',
-            'payment_address_1',
-            'payment_address_2',
-            'payment_city',
-            'payment_postcode',
-            'payment_country',
-            'payment_country_id',
-            'payment_zone',
-            'payment_zone_id',
-            'payment_address_format:ntext',
-            'payment_custom_field:ntext',
-            'payment_method',
-            'payment_code',
-            'shipping_firstname',
-            'shipping_lastname',
-            'shipping_company',
-            'shipping_address_1',
-            'shipping_address_2',
-            'shipping_city',
-            'shipping_postcode',
-            'shipping_country',
-            'shipping_country_id',
-            'shipping_zone',
-            'shipping_zone_id',
-            'shipping_address_format:ntext',
-            'shipping_custom_field:ntext',
-            'shipping_method',
-            'shipping_code',
-            'comment:ntext',
-            'total',
-            'order_status_id',
-            'affiliate_id',
-            'commission',
-            'marketing_id',
-            'tracking',
-            'language_id',
-            'currency_id',
-            'currency_code',
-            'currency_value',
-            'ip',
-            'forwarded_ip',
-            'user_agent',
-            'accept_language',
-            'date_added',
-            'date_modified',
-            'date_invoice',
-        ],
-    ]) ?>
+
+  <span class="pheader"> Products:</span>
+
+
+  <hr>
+
+  <table class="table">
+    <?php foreach ($parrs as $k => $v): ?>
+      <tr>
+        <td style="width:50%;border-top:0px">[Product code : <span class="products"> <?php echo $v['product_id'] ?></span>] <?php echo $v['quantity'] ?>x
+          <?php echo $v['name'] ?>
+        </td>
+        <?php $sums[] = $v['price'] ?>
+        <td style="width:25%;border-top:0px"><?php echo '$'.number_format($v['price'],2)  ?></td>
+        <td style="width:25%;border-top:0px">
+          <?php
+            $images = Product::find()->where(['product_id'=>$v['product_id']])->one();
+            $search_path = Yii::getAlias('@roots').'/image/'.$images->image;
+         ?>
+         <?php if (file_exists($search_path)): ?>
+           <?php $npath = Yii::getAlias('@image').'/'.$images->image; ?>
+           <img src="<?php echo $npath ?>" alt="" height:"auto" width="50%">
+          <?php else: ?>
+            <img src="<?php echo $defaults ?>" alt="">
+         <?php endif; ?>
+        </td>
+      </tr>
+    <?php endforeach; ?>
+
+
+  </table>
+
+  <hr>
+
+  <table class="table">
+    <tr>
+      <td style="width:50%;border-top:0px" class="rborder">SubTotal</td>
+      <td style="width:25%;border-top:0px" class="rborder"><?php echo '$'.number_format(array_sum($sums),2) ?></td>
+      <td style="border-top:0px"></td>
+    </tr>
+    <tr>
+      <td style="width:50%;border-top:0px">GST</td>
+      <td style="width:25%;border-top:0px">$0.00</td>
+      <td style="border-top:0px"></td>
+    </tr>
+    <tr>
+      <td style="width:50%;border-top:0px"><strong>Total Paid:</strong> </td>
+      <td style="width:25%;border-top:0px"><?php echo '$'.number_format($model->total,2)  ?></td>
+      <td style="border-top:0px"></td>
+    </tr>
+
+  </table>
+
+  <br>
+  <div class="row">
+    <div class="col-md-6">
+      <span class="pheader">Recipient's Info :</span>
+    </div>
+    <div class="col-md-6">
+      <span class="pheader">Delivery Date & Time :</span>
+    </div>
+  </div>
+  <hr>
+  <div class="row">
+    <div class="col-md-6">
+      <p><?php echo $model->shipping_firstname ?></p>
+      <p><?php echo $model->shipping_company ?></p>
+      <p><?php echo $model->shipping_address_1 ?></p>
+      <p><?php echo $model->shipping_postcode ?></p>
+      <p><?php echo $model->shipping_country ?></p>
+      <p>Tel: <?php echo $model->shipping_telephone ?></p>
+
+    </div>
+    <div class="col-md-6">
+      <strong>Delivery Date:</strong>   <?php echo date('l d M Y', strtotime($pone->delivery_date) ) ?>
+      <br>
+      <strong>Delivery time:</strong>   <?php echo $pone->delivery_text_time ?>
+    </div>
+  </div>
+
+
+  <br><br>
+  <div class="row">
+    <div class="col-md-6">
+      <span class="pheader">Sender's Info :</span>
+    </div>
+    <div class="col-md-6">
+      <span class="pheader">Gift Message :</span>
+    </div>
+  </div>
+  <hr>
+  <div class="row">
+    <div class="col-md-6">
+      <p><strong><?php echo $model->firstname ?></strong> </p>
+      <p><?php echo $model->payment_country ?></p>
+      <p>Email: <?= Html::mailto($model->email, $model->email) ?></p>
+      <p>Tel: <?php echo $model->telephone ?></p>
+      <p><strong><?php echo $model->order_source ?></strong> </p>
+    </div>
+    <div class="col-md-6">
+      <strong>To :</strong><?php echo $pone->gift_to ?>
+      <br>
+      <strong>Message :</strong> <?php echo  nl2br($pone->gift_message_comment) ?>
+      <strong>From:</strong> <?php echo $pone->gift_from ?>
+    </div>
+  </div>
+
+
 
 </div>
