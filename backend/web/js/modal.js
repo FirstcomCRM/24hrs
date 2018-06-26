8cbffd6eee9fdb55fab652aca693dcdf93843f02
Eumaser <eumerjoseph.ramos@yahoo.com>
2018-06-26 09:28:33 +0800

    addeded new modal class

--- backend/web/js/modal.js
+++ backend/web/js/modal.js
@@ -1,11 +1,24 @@
 $(function(){
 //  $('#modalButton').click(function(){ //#modalButton, changed to class when in gridview customer link
+
   $('.modalButton').click(function(e){
     e.preventDefault();
-    console.log('test');
+//    console.log('test');
     $('#modals').modal('show')
       .find('#modalContent')
       .load($(this).attr('value'));
+  });
+
+  $('.modalDelUp').click(function(e){
+    e.preventDefault();
+//    console.log('test');
+    $('#modalsDelUp').modal('show')
+      .find('#modalContentDel')
+      .load($(this).attr('value'));
+
+
+
+
   });
 
 });
