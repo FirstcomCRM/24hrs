a94fb9067f455d7c8bbff1d75ed0f04cbbcaa4c7
Eumaser <eumerjoseph.ramos@yahoo.com>
2018-06-26 09:30:58 +0800

    cron job

--- console/controllers/CronController.php
+++ console/controllers/CronController.php
@@ -14,18 +14,11 @@ class CronController extends Controller
 {
 
     public function actionSetCompleted(){
+      ini_set("memory_limit", "64M");
       $dates = new \DateTime();
-      $ndate = $dates->modify('-1 day')->format('Y-m-d');
-  //    Yii::$app->db->createCommand()->update('offline_order',['status'=>5],'status=1' )->execute();
+      $ndate = $dates->format('Y-m-d');
+    //  $ndate = $dates->modify('-1 day')->format('Y-m-d');
       Yii::$app->db->createCommand()->update('offline_order',['status'=>5],['status'=>1,'delivery_date'=>$ndate] )->execute();
-      //Yii::$app->db->createCommand()->update('order',['order_status_id'=>5],'order_status_id=1' )->execute();
-    /*  $data = Order::find()->where(['order_status_id'=>1])->orderBy(['order_id'=>SORT_DESC])->asArray()->all();
-      foreach ($data as $key => $value) {
-          $ndata = OrderProduct::find()->where(['order_id'=>$value['order_id'] ])->asArray()->one();
-          if ($ndate == $ndata['collection_date'] || $ndate == $ndata['delivery_date']) {
-              Yii::$app->db->createCommand()->update('order',['order_status_id'=>5],['order_id'=>$ndata['order_id'] ] )->execute();
-          }
-      }*/
 
       $query = new Query();
       $data = $query->select('a.order_id,a.order_status_id,b.delivery_date')
@@ -36,8 +29,9 @@ class CronController extends Controller
           ->all();
       foreach ($data as $key => $value) {
         Yii::$app->db->createCommand()->update('order',['order_status_id'=>5],['order_id'=>$value['order_id'] ] )->execute();
-
+        echo 'coode executed'.$ndate;
       }
+    //   echo 'success the run for reduc date '.$ndate.' then true date value is'.$cur->format('Y-m-d');
     }
 
 }
